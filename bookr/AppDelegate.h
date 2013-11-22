46e8ee10538b99ed445dc15eade2a13fa178b970
Steve Maahs <steve.maahs@piobyte.de>
2013-11-22 12:48:03 +0100

    init with Restkit

---
+++ bookr/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  bookr
+//
+//  Created by Steve Maahs on 06.11.13.
+//  Copyright (c) 2013 WSM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ bookr/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  bookr
+//
+//  Created by Steve Maahs on 06.11.13.
+//  Copyright (c) 2013 WSM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
